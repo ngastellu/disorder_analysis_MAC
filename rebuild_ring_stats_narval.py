957e3178dfcfea3b0c42a06895fa0bbd8bd0ac30
Nico <nicolas.gastellu@mail.mcgill.ca>
2024-11-04 13:56:47 -0500

    Ring stats from rebuild script now only operates on a single structure (instead of looping over all structures)

--- rebuild_ring_stats_narval.py
+++ rebuild_ring_stats_narval.py
@@ -2,28 +2,9 @@
 
 import numpy as np
 from ring_analysis_MPI import ring_stats_rebuild
-import os
+import sys
 
-structype = os.path.basename(os.getcwd())
-
-if structype == 'equil_tempdot5':
-    lbls = range(199)
-elif structype == 'equil_tempdot6':
-    lbls = range(200)
-elif structype == 'tempdot6':
-    lbls = range(218)
-elif structype == 'tempdot5':
-    lbls = range(217)
-else:
-    print(f'Invalid structype {structype}.')
-
-
-n = lbls[0]
+n = sys.argv[1]
 ring_stats = ring_stats_rebuild(f'sample-{n}', n)
 
-for n in lbls[1:]:
-    ring_stats += ring_stats_rebuild(f'sample-{n}', n)
-
-np.save(f'ring_stats_{structype}.npy', ring_stats)
-
-
+np.save(f'sample-{n}/ring_counts.npy', ring_stats)
